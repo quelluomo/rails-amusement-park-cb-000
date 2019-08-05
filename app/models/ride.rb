5ae77e0f780afc7ad80622d3f7d4e3f0b8be5d2d
Matteo Muscas <thatman.j@gmail.com>
2019-08-05 13:59:50 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -6,6 +6,7 @@ class Ride < ActiveRecord::Base
   #validates :user_id, :attraction_id, presence: true
 
   def take_ride
+
     if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
       "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
     elsif self.user.tickets < self.attraction.tickets
@@ -13,36 +14,24 @@ class Ride < ActiveRecord::Base
     elsif self.user.height < self.attraction.min_height
       "Sorry. You are not tall enough to ride the #{self.attraction.name}."
     else
-      ride_taken
-      "Thanks for riding the #{self.attraction.name}."
+      self.user.update(:tickets => new_tickets, :nausea => new_nausea, :happiness => new_happiness)
+      "Thanks for riding the #{self.attraction.name}!"
     end
   end
 
+  private
 
+    def new_tickets
+      self.user.tickets -= self.attraction.tickets
+    end
 
-  def poor
-    self.user.tickets < self.attraction.tickets
-  end
+    def new_nausea
+      self.user.nausea += self.attraction.nausea_rating
+    end
 
-  def short
-    self.user.height < self.attraction.min_height
-  end
-
-  def new_tickets
-    self.user.tickets -= self.attraction.tickets
-  end
-
-  def new_nausea
-    self.user.nausea += self.attraction.nausea_rating
-  end
-
-  def new_happiness
-    self.user.happiness += self.attraction.happiness_rating
-  end
-
-  def ride_taken
-    self.user.update(:tickets => new_tickets, :nausea => new_nausea, :happiness => new_happiness)
-  end
+    def new_happiness
+      self.user.happiness += self.attraction.happiness_rating
+    end
 
 
 end
